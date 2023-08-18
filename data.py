06a1ac26776137d2daa31c4580e89f3759be68fb
KoshCocna <76080450+KoshCocna@users.noreply.github.com>
2023-08-18 13:29:02 +1000

    Update data.py

--- data.py
+++ data.py
@@ -9,7 +9,6 @@ response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 question_data = response.json()['results']
 
-# question_data
 # question_data = [
 #     {
 #         "category": "Science: Computers",
